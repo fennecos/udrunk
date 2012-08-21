7287ee3d9f007c8524fcb798d95227ee8b5e67cf
Emmanuel <garnier.emmanuel@gmail.com>
2012-08-21 14:24:43 -0400

    Added Model class

---
+++ android/udrunk/src/net/udrunk/model/Model.java
@@ -0,0 +1,10 @@
+package net.udrunk.model;
+
+import com.googlecode.androidannotations.annotations.EBean;
+import com.googlecode.androidannotations.api.Scope;
+
+
+@EBean(scope = Scope.Singleton)
+public class Model {
+
+}
