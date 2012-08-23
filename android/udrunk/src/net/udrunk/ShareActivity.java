9b176eed802af99ebe32f40baa645cd06c8563bc
Emmanuel <garnier.emmanuel@gmail.com>
2012-08-23 17:49:09 -0400

    Enhanced ShareActivity fragment management

--- android/udrunk/src/net/udrunk/ShareActivity.java
+++ android/udrunk/src/net/udrunk/ShareActivity.java
@@ -5,64 +5,58 @@ import java.util.Observable;
 import net.udrunk.domain.Checkin;
 import net.udrunk.domain.Place;
 import net.udrunk.model.Model;
-
-
-
 import android.support.v4.app.FragmentTransaction;
 
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
-import com.googlecode.androidannotations.annotations.FragmentById;
 
 @EActivity(R.layout.share)
 public class ShareActivity extends CommonActivity {
 
-	@FragmentById(R.id.placesFragment)
-	PlacesFragment placesFragment;
-	@FragmentById(R.id.shareFragment)
-	ShareFragment shareFragment;
-	
+	private PlacesFragment placesFragment;
 	private Place currentPlace;
 
 	@AfterViews
 	public void afterViews() {
+		placesFragment = new PlacesFragment_();
+
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.add(R.id.fragment_container, placesFragment);
+		ft.commit();
+
 		placesFragment.updatePlaces();
-		
-		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();  
-		ft.hide(shareFragment);
-		ft.commit(); 
 	}
-	
+
 	@Override
 	public void update(Observable observable, Object data) {
-		if(data.equals( Model.PLACES_UPDATED ))
-		{
+		if (data.equals(Model.PLACES_UPDATED)) {
 			placesFragment.updatePlaces();
 		}
 	}
-	
-	public void showShareFragment(Place place)
-	{
+
+	public void showShareFragment(Place place) {
 		currentPlace = place;
-		
-		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();  
-		ft.hide(placesFragment);
-		ft.show(shareFragment);
-		ft.commit(); 
-		
+
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		// ft.setCustomAnimations(android.R.animator.fade_in,
+		// android.R.animator.fade_out);
+		ft.replace(R.id.fragment_container, new ShareFragment_());
+		ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+		ft.addToBackStack(null);
+		ft.commit();
+
 	}
 
-	public void sendCheckin(String message, int level)
-	{
+	public void sendCheckin(String message, int level) {
 		Checkin checkin = new Checkin();
-		
+
 		checkin.setPlace(currentPlace);
 		checkin.setLevel(level);
 		checkin.setStatus(message);
 		checkin.setUser(model.getCurrentUser());
-		
+
 		model.insertCheckin(checkin);
-		
-		onBackPressed();
+
+		finish();
 	}
 }
